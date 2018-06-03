040b2d6bc1636f402c53fd76f6682c7452519a26
zf-online <1158114251@qq.com>
2018-06-03 03:25:25 -0700

    add pram

--- 01-module-simple/hello.c
+++ 01-module-simple/hello.c
@@ -1,12 +1,12 @@
 #include <linux/module.h>
 #include <linux/init.h>
 
-/* 许可证声明 */
-MODULE_LICENSE("GPL");
+static char * dis_info = "hello driver";
+module_param(dis_info,charp,S_IRUGO);
 
 static int __init hello_init(void)
 {
-	printk("-- %s called --\n", __FUNCTION__);
+	printk("-- %s called INFO %s --\n", __FUNCTION__,dis_info);
 
 	return 0;
 }
@@ -19,5 +19,6 @@ static void __exit hello_exit (void)
 	return ;
 }
 
+MODULE_LICENSE("GPL");
 module_init(hello_init);
 module_exit(hello_exit);
